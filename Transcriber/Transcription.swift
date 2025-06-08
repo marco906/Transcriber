8fef005069c76433b3b86e873426c360d41303df
Marco Wenzel <mar.wenzel@gmx.de>
2025-06-08 10:53:30 +0200

    update transcription view

--- Transcriber/Transcription.swift
+++ Transcriber/Transcription.swift
@@ -5,14 +5,12 @@
 //  Created by Marco Wenzel on 29.05.25.
 //
 
+import Foundation
+
 struct Transcription: Identifiable, Equatable, Hashable {
-	var id: Int
+    var id: UUID = UUID()
+	var speakerId: Int
 	var start: Float
 	var end: Float
 	var text: String
-}
-
-extension Transcription {
-	static var preview1 = Transcription(id: 0, start: 0, end: 5, text: "Hello there I am speaker one! Some very long text that spans over multiple lines")
-	static var preview2 = Transcription(id: 1, start: 5, end: 10, text: "Hi, I am the secon speaker! Some very long text that spans over multiple lines")
 }
--- Transcriber/TranscriptionView.swift
+++ Transcriber/TranscriptionView.swift
@@ -8,21 +8,43 @@
 import SwiftUI
 
 struct TranscriptionView: View {
-	let transcription: Transcription
-	
-	init(_ transcription: Transcription) {
-		self.transcription = transcription
-	}
-	
-    var body: some View {
-		Text(transcription.text)
+    let transcription: Transcription
+    
+    init(_ transcription: Transcription) {
+        self.transcription = transcription
     }
-}
-
-#Preview {
-	NavigationStack {
-		VStack {
-			TranscriptionView(.preview1)
-		}
-	}
+    
+    var trailingAlignment: Bool {
+        transcription.speakerId == 0 ? true : false
+    }
+    
+    var color: Color {
+        transcription.speakerId == 0 ? .teal.opacity(0.3) : .gray.opacity(0.30)
+    }
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            VStack(alignment: trailingAlignment ? .trailing : .leading) {
+                Text("Speaker \(transcription.speakerId)")
+                    .font(.subheadline)
+                    .fontWeight(.medium)
+                    .foregroundStyle(.secondary)
+                    .padding(.bottom, 4)
+                VStack(alignment: .trailing, spacing: 8) {
+                    Text(transcription.text)
+                    Text(String(format: "% 2.0fs", transcription.start))
+                        .font(.footnote)
+                        .foregroundStyle(.secondary)
+                }
+                .padding(.vertical, 8)
+                .padding(.horizontal, 12)
+                .background(
+                    RoundedRectangle(cornerRadius: 10)
+                        .foregroundStyle(color)
+                )
+            }
+            .frame(maxWidth: .infinity, alignment: trailingAlignment ? .trailing : .leading)
+        }
+        .padding(.bottom)
+    }
 }
